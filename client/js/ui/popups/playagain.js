da68e464e71de2ea96f22c8dd37c5502cb103d65
horacio <hmk142@hotmail.com>
2017-04-05 04:47:53 -0300

    play again menu

--- client/js/ui/popups/playagain.js
+++ client/js/ui/popups/playagain.js
@@ -7,13 +7,17 @@ define(["text!../../../menus/playAgain.html!strip", 'ui/popups/popup'], function
   class playAgain extends PopUp {
     constructor(game) {
       var options = {
-        width: 300,
-        height: 400,
+        width: 220,
+        height: 240,
         minWidth: 150,
-        minHeight: 250
+        minHeight: 200
       };
       super(DOMdata, options);
       this.game = game;
+      this.$playAgain = $("#playAgain");
+      this.$changeCharacter = $("#changeCharacter");
+      this.$back = $("#playAgainBack");
+
       this.initCallbacks();
     }
 
@@ -22,7 +26,21 @@ define(["text!../../../menus/playAgain.html!strip", 'ui/popups/popup'], function
     }
 
     initCallbacks() {
-      var self = this;
+      this.$playAgain.click(() => {
+
+        this.hide();
+      });
+
+      this.$changeCharacter.click(() => {
+
+        this.hide();
+      });
+
+      this.$back.click(() => {
+        this.game.client.sendQuit();
+        this.hide();
+      });
+
     }
   }
   return playAgain;
